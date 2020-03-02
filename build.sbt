69facb5cf889c7906cc366fcd7f844ca22ae1fd3
Ravinder Chauhan <chuhrav@amazon.com>
2020-03-02 11:53:22 +0530

    Initial Commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "ProjectTest"
+
+version := "0.1"
+
+scalaVersion := "2.13.1"
---
+++ src/main/scala/Shapes.scala
@@ -0,0 +1,5 @@
+object Shapes {
+  def main(args: Array[String]): Unit = {
+    println("HEY COOL DUDE")
+  }
+}
